a6a46c44618e592f849998452a5a46f0f8ece5ea
Temirlan <76694765+a6eu@users.noreply.github.com>
2025-02-26 01:22:22 +0500

    Update swagger.js

--- swagger.js
+++ swagger.js
@@ -11,7 +11,7 @@ const options = {
         },
         servers: [
             {
-                url: 'http://localhost:4000', 
+                url: 'https://jedel-jardem-production.up.railway.app', 
             },
         ],
         components: {
